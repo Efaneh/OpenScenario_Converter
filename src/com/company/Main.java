d4c48a4194ebdda5d9a032707296ad0b68017a35
Efan Haynes <efanhaynes@gmail.com>
2021-10-06 20:46:56 +0100

    Added pdf for reference

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Parser parseLog = new Parser(args[0]);
-        Writer openSWriter = new Writer(parseLog.getLogLines(), args[1], args[2], parseLog, 2);
+        try {
+            Parser parseLog = new Parser(args[0]);
+            Writer openSWriter = new Writer(parseLog.getLogLines(), args[1], args[2], parseLog, 2);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Incorrect command line parameters - Should be Log file (txt), name of produced file and finally the CARLA map");
+        }
     }
 }
